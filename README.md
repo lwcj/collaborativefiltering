afa77188bfe14c016aff4db864831b19aa748d60
Jordan Leung <jl4700@columbia.edu>
2017-12-01 15:15:16 -0500

    added algos, data and update readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# collaborativefiltering
+# Collaborative Filtering
+
+Two algorithms I coded as part of Ying Liu's Fall 2017 Applied Data Science course at Columbia.
+
+SimRank is coded in R, and based on [this paper](http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.578.8723&rep=rep1&type=pdf) by Glen Jeh and Jennifer Widom, and also based on Colin Su's [Python implementation](https://github.com/littleq0903/simrank).
+
+Mean Squared Difference was coded in Python, and was optimized for parallel processing.
+
+The data should be a sparse matrix. For SimRank the data should be 1s and 0s. For Mean Squared Difference, the data can be the scores.
---
+++ simrank.R
@@ -0,0 +1,117 @@
+# returns the corresponding row or column for a user or movie.
+get_movies_num <- function(user){
+  u_i <- match(user, users)
+  return(graph[u_i,-1])
+}
+
+get_users_num <- function(movie){
+  m_j <- match(movie, movies)
+  return(graph[,m_j+1])
+}
+
+# return the users or movies with a non zero
+get_movies <- function(user){
+  series = get_movies_num(user)
+  return(movies[which(series!=0)])
+}
+
+get_users <- function(movie){
+  series = get_users_num(movie)
+  return(users[which(series!=0)])
+}
+
+user_simrank <- function(u1, u2, C) {
+  if (u1 == u2){
+    return(1)
+  } else {
+  pre = C / (sum(get_movies_num(u1)) * sum(get_movies_num(u2)))
+  post = 0
+  for (m_i in get_movies(u1)){
+    for (m_j in get_movies(u2)){
+      i <- match(m_i, movies)
+      j <- match(m_j, movies)
+      post <- post + movie_sim[i, j]
+    }
+  }
+  return(pre*post)
+  }
+}
+
+movie_simrank <- function(m1, m2, C) {
+  if (m1 == m2){
+    return(1)
+  } else {
+    pre = C / (sum(get_users_num(m1)) * sum(get_users_num(m2)))
+    post = 0
+    for (u_i in get_users(m1)){
+      for (u_j in get_users(m2)){
+        i <- match(u_i, users)
+        j <- match(u_j, users)
+        post <- post + user_sim[i, j]
+      }
+    }
+    return(pre*post)
+  }
+}
+
+simrank <- function(C=0.8, times = 1){
+  
+  for (run in 1:times){
+    
+    if(calc_user){
+    for (ui in users){
+      for (uj in users){
+        i = match(ui, users)
+        j = match(uj, users)
+        user_sim[i, j] <<- user_simrank(ui, uj, C)
+      }
+    }
+    }
+    if(calc_movie){
+    for (mi in movies){
+      for (mj in movies){
+        i = match(mi, movies)
+        j = match(mj, movies)
+        movie_sim[i, j] <<- movie_simrank(mi, mj, C)
+      }
+    }
+    }
+  }
+}
+
+# formatting training dataset
+
+graph <- read.csv('sampledata.csv')
+
+# convert to 0s and 1s, threshold is set at 4.
+
+graph[,-1][graph[,-1] < 5] <- 0
+graph[,-1][graph[,-1] >= 5] <- 1
+
+
+# set similarity matrices to be calculated
+calc_user = T
+calc_movie = F
+
+# initialize the similarity matrices
+user_sim <- diag(dim(graph)[1])
+movie_sim <- diag(dim(graph)[2])
+
+# create list of users and movies
+users <- graph[,1]
+movies <- colnames(graph[,-1])
+
+simrank(0.8, 1)
+
+if(calc_user){
+colnames(user_sim) <- users
+user_sim <- cbind(users, user_sim)
+write.csv(user_sim, file='usersim.csv', row.names = FALSE)
+}
+
+if(calc_movie){
+  colnames(movie_sim) <- movies
+  movie_sim <- cbind(movies, movie_sim)
+  write.csv(movie_sim, file='moviesim.csv', row.names = FALSE)
+}
+
